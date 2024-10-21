82fc23062d1a9b7a6cc1275067e79c8d05893679
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2024-10-21 23:47:42 +0200

    extract board

--- src/routes/(authenticated)/app/dashboard/+page.svelte
+++ src/routes/(authenticated)/app/dashboard/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-	import { flip } from 'svelte/animate';
-	import { send, receive } from '$lib/utils/animation';
-	import TaskCard from '$lib/components/task-board/TaskCard.svelte';
 	import type { Task } from '$lib/types/tasks';
-	import StatusColumn from '$lib/components/task-board/StatusColumn.svelte';
-
-	let filterText = $state('');
+	import Board from '$lib/components/task-board/Board.svelte';
 
 	let tasks: Task[] = $state([
 		{
@@ -69,92 +64,6 @@
 			updatedAt: new Date()
 		}
 	]);
-
-	let filteredTasks = $derived(
-		tasks.filter(
-			(task) =>
-				task.title.toLowerCase().includes(filterText.toLowerCase()) ||
-				task.description.toLowerCase().includes(filterText.toLowerCase())
-		)
-	);
-	let sortedTasks = $derived(
-		filteredTasks.toSorted((a, b) => a.updatedAt.getTime() - b.updatedAt.getTime())
-	);
-	let openTodos = $derived(sortedTasks.filter((task) => task.status === 'todo'));
-	let inProgressTodos = $derived(sortedTasks.filter((task) => task.status === 'in-progress'));
-	let doneTodos = $derived(sortedTasks.filter((task) => task.status === 'done'));
-
-	function ondragstart(event: DragEvent) {
-		event.dataTransfer!.setData('text/plain', event.target!.id);
-	}
-
-	function ondragover(event: DragEvent) {
-		event.preventDefault();
-		event.dataTransfer!.dropEffect = 'move';
-	}
-
-	function ondrop(event: DragEvent) {
-		event.preventDefault();
-		const id = event.dataTransfer!.getData('text/plain');
-
-		const task = tasks.find((task) => task.id === id);
-
-		if (!task) return;
-
-		task.status = event.currentTarget!.dataset.status!;
-		task.updatedAt = new Date();
-	}
 </script>
 
-<article class="board">
-	<header class="board-header">
-		<h1>Task Board</h1>
-		<form role="search">
-			<input name="filter" type="search" placeholder="Filter" bind:value={filterText} />
-		</form>
-	</header>
-	<div class="grid">
-		<StatusColumn status="todo" {ondragover} {ondrop}>
-			{#each openTodos as task (task.id)}
-				<div animate:flip in:send={{ key: task.id }} out:receive={{ key: task.id }}>
-					<TaskCard draggable="true" {task} {ondragstart} />
-				</div>
-			{/each}
-		</StatusColumn>
-		<StatusColumn status="in-progress" {ondragover} {ondrop}>
-			{#each inProgressTodos as task (task.id)}
-				<div animate:flip in:send={{ key: task.id }} out:receive={{ key: task.id }}>
-					<TaskCard draggable="true" {task} {ondragstart} />
-				</div>
-			{/each}
-		</StatusColumn>
-		<StatusColumn status="done" {ondragover} {ondrop}>
-			{#each doneTodos as task (task.id)}
-				<div animate:flip in:send={{ key: task.id }} out:receive={{ key: task.id }}>
-					<TaskCard draggable="true" {task} {ondragstart} />
-				</div>
-			{/each}
-		</StatusColumn>
-	</div>
-</article>
-
-<style>
-	.board {
-		margin: calc(var(--pico-spacing) * 3) auto;
-		max-width: calc(100% - var(--pico-spacing) * 6);
-
-		header {
-			display: grid;
-			grid-template-columns: 2fr 1fr;
-			align-items: center;
-
-			> * {
-				margin-bottom: 0;
-			}
-		}
-
-		.grid > * {
-			margin-bottom: 0;
-		}
-	}
-</style>
+<Board {tasks} title="Task Board" />
